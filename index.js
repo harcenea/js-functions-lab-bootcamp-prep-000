a481a4609a56c4d61de2394b32631f97046f0d61
Hugh A <harcenea@gmail.com>
2018-06-01 14:10:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 function happyHolidays(){return "Happy holidays!";}
 
-function happyHolidaysTo(name) {return `Happy holidays, ${name}!`;}
+function happyHolidaysTo(name) {return `Happy holidays,  ${name}!`;}
 
 function happyHolidayTo(holiday, name) {`Happy ${holiday}, ${name}!`;}
 
